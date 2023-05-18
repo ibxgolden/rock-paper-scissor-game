58cf53fa2f9fce1aee1e3fd98bf9b276de1de44e
ibxgolden <129530843+ibxgolden@users.noreply.github.com>
2023-05-18 10:09:46 +0300

    Update abc.js
    geting the user input and comparing for the win possiblities

--- abc.js
+++ abc.js
@@ -12,3 +12,18 @@ const choices = ['rock', 'paper', 'scissors'];
 const randomNumber = Math.floor(Math.random() * 3);
 return choices[randomNumber];
 }
+function convertCase(anythingIwant) {
+if (anythingIwant === 'paper') return 'Paper';
+if (anythingIwant === 'scissors') return 'Scissors';
+return 'Rock';
+}
+function win(user, computer) {
+userScore++;
+userScore_span.innerHTML = userScore;
+const userName = ' (user)'.fontsize(3).sup();
+const compName = ' (comp)'.fontsize(3).sup();
+result_div.innerHTML = `<p>${convertCase(user)}${userName} beats ${convertCase(computer)}${compName}. You win!</p>`;
+const roundStatus = document.getElementById(user);
+roundStatus.classList.add('winningStyles');
+setTimeout(() => roundStatus.classList.remove('winningStyles'), 300);
+}
